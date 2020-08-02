4831855c2c2f3216997b62f154ec2ec543782345
sathyadevops476 <sathyadevops476@gmail.com>
2020-08-02 18:37:30 +0530

    animals - initial commit

---
+++ animals.py
@@ -0,0 +1,20 @@
+#/usr/bin/python
+
+import sys
+
+print("animals")
+
+animal=sys.argv[1]
+
+if(animal == "Cat"):
+    print(animal)
+elif(animal == "Dog"):
+    print(animal)
+else:
+    print("No specific animals")
+
+
+
+
+
+
